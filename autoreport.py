a26b418e205e2a3d0a5d913285c74e436b691577
RyanMattera <ryan.mattera@live.com>
2019-01-21 11:46:29 -0500

    Update autoreport.py

--- autoreport.py
+++ autoreport.py
@@ -1,27 +1,28 @@
-#autoreport.py
-#Working to take the data from a csv file and create a report that is then emailed to relevant parties
-
 from jinja2 import FileSystemLoader, Environment
 
-#content to be put in the report
-content = "Hello, World!"
+# Content to be published
+content = "Hello, world!"
 
-#Configures Jinja and ready the template
-env = Environment (
-	loader=FileSystemLoader(searchpath="templates")
-	)
+# Configure Jinja and ready the template
+env = Environment(
+    loader=FileSystemLoader(searchpath="templates")
+)
 template = env.get_template("report.html")
 
+
 def main():
-	"""
-	Entry Point for the script
-	Render a template and write to file
-	:return:
-	"""
-	with open("outputs/report.html", "w") as f:
-	f.write(template.render(content=content))
-	
-	if __name__ == "__main__":
-		main()
+    """
+    Entry point for the script.
+    Render a template and write it to file.
+    :return:
+    """
+    with open("outputs/report.html", "w") as f:
+        f.write(template.render(content=content))
+
+
+if __name__ == "__main__":
+    main()
+
+
 		
 		
