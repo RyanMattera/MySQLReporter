2fdcabf445dc1d903fdab3ce32094f50ed9ecdc0
RyanMattera <ryan.mattera@live.com>
2019-01-21 16:09:12 -0500

    Update autoreport.py

--- autoreport.py
+++ autoreport.py
@@ -1,7 +1,21 @@
+import pandas as pd
+from jinja2 import FileSystemLoader, Environment
+
 from datetime import date
 today = str(date.today())
 
-from jinja2 import FileSystemLoader, Environment
+#Allow for very wide columns - otherwise columns are spaced and ellipse'd
+pd.set_option("display.max_colwidth", 200)
+
+def csv_to_html(filepath):
+	"""
+	Open a .csv and put in a HTML format.
+	:param filepath: Filepath to a .csv to be read
+	:return: String of HTML to be published.
+	"""
+	df = pd.read_csv(filepath, index_col=0)
+	html = df.to_html()
+	return html
 
 # Configure Jinja and ready the template
 env = Environment(
@@ -12,33 +26,32 @@ env = Environment(
 base_template = env.get_template("report.html")
 table_section_template = env.get_template("table_section.html")
 
-# Content to be published
-title = "Daily Report For %s" %today
-sections = list()
-sections.append(table_section_template.render(
-department ="Curing",
-dataset = "CuringReport.csv",
-table ="Table Goes here."
-))
-sections.append(table_section_template.render(
-department ="Deboning",
-dataset = "DeboningReport.csv",
-table ="Table Goes here."
-))
-
-
-
 def main():
     """
     Entry point for the script.
     Render a template and write it to file.
     :return:
     """
-    with open("outputs/report.html", "w") as f:
-        f.write(base_template.render(
-		title = title,
-		sections = sections
-		))
+
+# Content to be published
+title = "Daily Report For %s" %today
+sections = list()
+sections.append(table_section_template.render(
+department ="Curing",
+dataset = "CuringReport.csv",
+table =csv_to_html("datasets/CuringReport.csv")
+))
+sections.append(table_section_template.render(
+department ="Deboning",
+dataset = "DeboningReport.csv",
+table =csv_to_html("datasets/DeboningReport.csv")
+))
+
+with open("outputs/report.html", "w") as f:
+    f.write(base_template.render(
+	title = title,
+	sections = sections
+	))
 
 
 if __name__ == "__main__":
